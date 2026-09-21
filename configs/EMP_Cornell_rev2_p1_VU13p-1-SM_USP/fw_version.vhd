de3ca11a257972765bd9d5da9c35293c38ef2310
Konstantinos Alexopoulos <k.alexopoulos@cern.ch>
2026-09-21 23:53:30 +0200

    continuous_reconfiguration : changed accepting axi addresses  for croc reconfig

--- configs/EMP_Cornell_rev2_p1_VU13p-1-SM_USP/fw_version.vhd
+++ configs/EMP_Cornell_rev2_p1_VU13p-1-SM_USP/fw_version.vhd
@@ -5,10 +5,10 @@ package FW_TIMESTAMP is
   constant TS_CENT     : std_logic_vector(7 downto 0) := x"20";
   constant TS_YEAR     : std_logic_vector(7 downto 0) := x"26";
   constant TS_MONTH    : std_logic_vector(7 downto 0) := x"09";
-  constant TS_DAY      : std_logic_vector(7 downto 0) := x"03";
+  constant TS_DAY      : std_logic_vector(7 downto 0) := x"20";
   constant TS_HOUR     : std_logic_vector(7 downto 0) := x"12";
-  constant TS_MIN      : std_logic_vector(7 downto 0) := x"29";
-  constant TS_SEC      : std_logic_vector(7 downto 0) := x"17";
+  constant TS_MIN      : std_logic_vector(7 downto 0) := x"03";
+  constant TS_SEC      : std_logic_vector(7 downto 0) := x"25";
 end package FW_TIMESTAMP;
  
  
@@ -17,11 +17,11 @@ use ieee.std_logic_1164.all;
 -- fw version package
 package FW_VERSION is
   constant FW_HASH_VALID : std_logic                      := '1';
-  constant FW_HASH_1     : std_logic_vector(31 downto  0) := x"e07a1e17";
-  constant FW_HASH_2     : std_logic_vector(31 downto  0) := x"c580e32b";
-  constant FW_HASH_3     : std_logic_vector(31 downto  0) := x"6b7b1da1";
-  constant FW_HASH_4     : std_logic_vector(31 downto  0) := x"83ee554c";
-  constant FW_HASH_5     : std_logic_vector(31 downto  0) := x"47704024";
+  constant FW_HASH_1     : std_logic_vector(31 downto  0) := x"585434fb";
+  constant FW_HASH_2     : std_logic_vector(31 downto  0) := x"f4548b77";
+  constant FW_HASH_3     : std_logic_vector(31 downto  0) := x"48c5f28d";
+  constant FW_HASH_4     : std_logic_vector(31 downto  0) := x"14af4450";
+  constant FW_HASH_5     : std_logic_vector(31 downto  0) := x"1effed7f";
 end package FW_VERSION;
  
  
